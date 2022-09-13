3d5ef98ece823436d6339a86f9d6e08f19ff3891
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-13 23:49:52 +0300

    add data_binding

--- app/src/main/java/com/example/simplyapp/MainActivity.kt
+++ app/src/main/java/com/example/simplyapp/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.example.simplyapp
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.simplyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.submitButton.setOnClickListener {
             greeting()
         }
     }
 
 
-    private fun greeting(){
-        val messageView = findViewById<TextView>(R.id.textView)
-        val nameText = findViewById<TextView>(R.id.name_edit_text)
-        messageView.text = "Hello " + nameText.text
+    private fun greeting() {
+        binding.apply {
+            greetingTextView.text = "Hello " + nameEditText.text
+        }
+
     }
 }
